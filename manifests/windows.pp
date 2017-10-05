5e0f87d35955a2e2abcaf88ff896560e609c1f8c
Costa Argiris <costa.argiris@lark-it.com>
2017-10-05 11:33:40 -0600

    Updated windows profile to add user and group

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -1,13 +1,14 @@
 # /etc/puppetlabs/code/environments/<#ENVIRONMENT>/profile/manifests/mysql/master.pp
 class profile::windows {
 
-  $mysql_root_passwd = lookup('profile::mysql::root_password')
-  $mysql_dbs = lookup('profile::mysql::mysql_dbs', Hash, 'deep', {})
-
-  class { '::mysql::server':
-    root_password           => $mysql_root_passwd,
-    remove_default_accounts => false,
+  group { 'sdpusers':
+    ensure => present,
+    gid    => '111',
   }
-  
-  create_resources('::mysql::db',$mysql_dbs)
+
+  user { 'costa':
+    ensure  => present,
+    gid     => '111', 
+    require => Group['sdpusers'],
+  }
 }
